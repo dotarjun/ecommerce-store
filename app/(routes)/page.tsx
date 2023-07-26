82a3be5e8b7a2cf3fdf33f32d169fda2371374de
Arjun <arjunsingh8112@gmail.com>
2023-07-26 09:05:40 +0530

    feat: build home page with Billboard fetched from API

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,4 +1,18 @@
-const HomePage = () => {
-  return <div>Hello Store</div>;
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("3cdcd0a5-8649-4f5e-a614-34b8b00622d7");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 export default HomePage;
