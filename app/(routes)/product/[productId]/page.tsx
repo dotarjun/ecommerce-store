8f76e9fb20a57ae5a9801cf8e7a6a2460ac165e8
Arjun <arjunsingh8112@gmail.com>
2023-07-27 09:43:21 +0530

    bug: pass in product

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -16,14 +16,13 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
   const suggestedProducts = await getProducts({
     categoryId: product?.category?.id,
   });
-
   return (
     <div className="bg-white">
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
             <div>
-              <Gallery images={product.images} />
+              <Gallery product={product} />
             </div>
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">Info</div>
           </div>
