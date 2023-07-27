52233fb591f8c15f32458d65b0713c6ded1b4b19
Arjun <arjunsingh8112@gmail.com>
2023-07-27 09:43:35 +0530

    bug: ts ignore for now

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -22,6 +22,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
             <div>
+              {/* @ts-ignore */}
               <Gallery product={product} />
             </div>
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">Info</div>
