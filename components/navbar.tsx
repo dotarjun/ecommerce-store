9c359a853245fae829a5526387c585668dffd910
Arjun <arjunsingh8112@gmail.com>
2023-07-28 15:21:38 +0530

    ux: add icon

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import Container from "@/components/ui/container";
 import getCategories from "@/actions/get-categories";
@@ -15,7 +16,14 @@ const Navbar = async () => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">Guitar Gear Shop</p>
+            <Image
+              src="/electric-guitar.webp"
+              alt="Guitar Gear Shop Icon"
+              width="30"
+              height="30"
+              className="w-[30px] h-[30px] hidden sm:block"
+            />
+            <p className="font-bold text-xl ">Guitar Shop</p>
           </Link>
           <div className="ml-auto flex items-center">
             <MobileNav data={categories} />
