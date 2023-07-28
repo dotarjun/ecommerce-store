fba81ab9911f870a9f449f2a4318e0535e0bbc4f
Arjun <arjunsingh8112@gmail.com>
2023-07-28 13:23:23 +0530

    feat: add to cart from product page

--- components/info.tsx
+++ components/info.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { MouseEventHandler } from "react";
+
 import { Product } from "@/types";
 import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/button";
 import { ShoppingCart } from "lucide-react";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
   data: Product[];
@@ -9,6 +14,13 @@ interface InfoProps {
 
 const Info: React.FC<InfoProps> = ({ data }) => {
   const [infoData] = data;
+  const cart = useCart();
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    cart.addItem(infoData);
+  };
 
   return (
     <div>
@@ -31,7 +43,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
             style={{ backgroundColor: infoData?.color?.value }}></div>
         </div>
         <div className="mt-10 flex items-center gap-x-3">
-          <Button className="flex items-center gap-x-2">
+          <Button onClick={onAddToCart} className="flex items-center gap-x-2">
             Add to Cart
             <ShoppingCart />
           </Button>
