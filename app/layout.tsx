a1711ca03f716cf9ef44df528b9adb5c13d995d1
dotarjun <arjunsingh8112@gmail.com>
2023-11-10 18:33:24 +0530

    feat: add metadata
    Signed-off-by: dotarjun <arjunsingh8112@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,22 @@ import ToastProvider from "@/providers/toast-provider";
 const font = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Guitar Store",
-  description: "Store",
+  title: "Guitar Gear Shop",
+  description:
+    "GGS is the best place to buy acoustic, electric, and bass guitars online. We carry all major brands and free shipping is available.",
+  keywords: ["guitars", "acoustic", "electric", "bass"],
+  authors: [
+    {
+      name: "Arjun Singh",
+      url: "https://arjunsingh.tech",
+    },
+  ],
+  openGraph: {
+    title: "Guitar Gear Shop",
+    description: "Buy the best guitars online",
+    url: "https://guitargear.shop",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
